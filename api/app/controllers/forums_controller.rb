11c12f62941ab0313854eff46929ea4f21c7da67
JosepRivaille <josep.de.cid@gmail.com>
2016-11-05 23:05:10 +0100

    Create Forums controller and model

---
+++ api/app/controllers/forums_controller.rb
@@ -0,0 +1,23 @@
+class ForumsController < ApplicationController
+
+  # GET /forums
+  def index
+    @forums = Forum.all
+  end
+
+  # GET /forums/:forumName
+  def show
+    @forum = Forum.find(params[:forumName])
+  end
+
+  # POST /forums/:forumName
+  def create
+
+  end
+
+  # PATCH /forums/:forumName
+  def update
+
+  end
+
+end
