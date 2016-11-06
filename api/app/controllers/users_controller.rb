907597ec9d916457d59a4c4c7b09586e6138e7ab
JosepRivaille <josep.de.cid@gmail.com>
2016-11-06 20:17:07 +0100

    Create users register first version

--- api/app/controllers/users_controller.rb
+++ api/app/controllers/users_controller.rb
@@ -2,9 +2,16 @@ class UsersController < ApplicationController
 
   #POST /users/:idTag
   def create
-
-    @user = User.new
-    @user.idTag
+    if request.get?
+      id_tag = [params[:idTag]]
+      @user = User.new
+      @user.idTag = id_tag
+      if @user.save
+        render :nothing => true, :status => :ok
+      else
+        render :nothing => true, :status => :internal_server_error
+      end
+    end
   end
 
 end
