fc464f55837d25c478fa0722816c9dc7afe0792f
JosepRivaille <josep.de.cid@gmail.com>
2016-11-06 11:52:21 +0100

    Create shou route example on forums

--- api/app/controllers/forums_controller.rb
+++ api/app/controllers/forums_controller.rb
@@ -7,7 +7,20 @@ class ForumsController < ApplicationController
 
   #GET /forums/:forumName
   def show
-    @forum = Forum.find(params[:forumName])
+    forum = {
+        name: 'The lord of the rings',
+        visitors: 1,
+        participants: 1,
+        entertainment: {
+            description: 'Book written by Tolkien.',
+            category: 'Books'
+        }
+    }
+    respond_to do |format|
+      format.json do
+        render json: { forum: forum }
+      end
+    end
   end
 
   #POST /forums/:forumName
