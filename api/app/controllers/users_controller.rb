5663964abfa4cdadaa68b262ecdf9fa4d8e25fe5
JosepRivaille <josep.de.cid@gmail.com>
2016-11-06 20:29:44 +0100

    Modified parameter to be in post

--- api/app/controllers/users_controller.rb
+++ api/app/controllers/users_controller.rb
@@ -1,16 +1,14 @@
 class UsersController < ApplicationController
 
-  #POST /users/:idTag
+  #POST /users
   def create
-    if request.get?
-      id_tag = [params[:idTag]]
-      @user = User.new
-      @user.idTag = id_tag
-      if @user.save
-        render :nothing => true, :status => :ok
-      else
-        render :nothing => true, :status => :internal_server_error
-      end
+    id_tag = [params[:idTag]]
+    @user = User.new
+    @user.add_id_tag(id_tag)
+    if @user.save
+      head :status => :ok
+    else
+      head :status => :internal_server_error
     end
   end
 
